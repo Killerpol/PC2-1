1b42f2df8650f48d2980b8898648ee2db52f0228
Ingrid A <ingrid10arellano@gmail.com>
2018-11-20 08:21:06 -0500

    Pregunta 3 resuelta

---
+++ pregunta3.py
@@ -0,0 +1,13 @@
+#Construya una aplicación que lea una matriz de datos (n * n) y calcule la suma de elementos
+#ubicados en la diagonal principal:
+#Los elementos de la matriz pueden ser establecidos por la aplicación.
+
+matriz = [(1,2,0),(3,1,4),(3,0,1)]
+f0C0 = matriz[0][0]
+f2C2 = matriz[1][1]
+f3C3 = matriz[2][2]
+suma= f0C0 + f2C2 + f3C3
+
+print (matriz)
+print (suma)
+
